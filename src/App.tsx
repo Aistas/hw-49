c5fd4356b0889f70695ca7102725de3aa01104c8
Aivar Bakirov <aivar2s1bakirov@gmail.com>
2024-05-28 12:08:15 +0600

    Init App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react';
+import Board from './components/Board/Board';
+
+const App: React.FC = () => {
+  return (
+      <div className="App">
+        <Board />
+      </div>
+  );
+};
+
+export default App;
